3c2f2db7551f67b339d29c2d68b65f22a897f587
Greg <gheidorn@cars.com>
2016-03-28 12:19:07 -0500

    repackaged

--- src/test/java/demo/messaging/MessageReaderApplicationTests.java
+++ src/test/java/demo/messaging/MessageReaderApplicationTests.java
@@ -1,5 +1,6 @@
 package demo.messaging;
 
+import demo.messaging.consumer.MessageReaderApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
